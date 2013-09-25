117a2cbf594f419d596a26c92838ec5967934d7d
Ivan Malison <IvanMalison@gmail.com>
2013-09-25 13:09:05 -0700

    Add no-easy-keys.

--- init.el
+++ init.el
@@ -52,6 +52,10 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
+;; Don't allow arrow keys
+(require 'no-easy-keys)
+(no-easy-keys 1)
+
 ;; Change the behavior of M-<arrow> so that it stops on underscores.
 (defun change-major-mode-hook () (modify-syntax-entry ?_ "_"))
 (setq c-subword-mode t)
@@ -78,9 +82,9 @@
   (interactive "r")
   (shell-command-on-region b e "cat | tmux loadb -"))
 
-;; ;; =============================================================================
-;; ;;                                                                       Flymake
-;; ;; =============================================================================
+;; =============================================================================
+;;                                                                       Flymake
+;; =============================================================================
 
 (require 'flymake)
 (require 'flymake-cursor)
@@ -95,7 +99,7 @@
 
 (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-pylint-init))
 
-;; Load flymake on non-temp buffers
+; Load flymake on non-temp buffers
 (add-hook 'python-mode-hook
 	  (lambda () (unless (eq buffer-file-name nil) (flymake-mode 1))))
 
@@ -107,6 +111,14 @@
 (require 'multi-line-it)
 (require 'emacs-testify)
 
+(defun python-tabs ()
+  (setq tab-width 4
+        indent-tabs-mode t
+        python-indent-offset 4))
+(python-tabs)
+
+;; Yelp always uses tabs.
+(add-hook 'python-mode-hook 'python-tabs)
 (add-hook 'python-mode-hook (lambda () (subword-mode 1)))
 
 ;; =============================================================================
@@ -114,22 +126,28 @@
 ;; =============================================================================
 
 ;; Fast cursor movement in vertical direction with Meta.
-(global-set-key (kbd "M-<down>") (lambda () (interactive) (next-line 5)))
-(global-set-key (kbd "M-<up>") (lambda () (interactive) (previous-line 5)))
-(global-set-key (kbd "ESC <down>") (lambda () (interactive) (next-line 5)))
-(global-set-key (kbd "ESC <up>") (lambda () (interactive) (previous-line 5)))
+(global-set-key (kbd "M-n") (lambda () (interactive) (next-line 5)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (previous-line 5)))
+(global-set-key (kbd "ESC n") (lambda () (interactive) (next-line 5)))
+(global-set-key (kbd "ESC p") (lambda () (interactive) (previous-line 5)))
+
+;; Macros
+(fset 'ipdb "import ipdb; ipdb.set_trace()")
 
 ;; Miscellaneous
 (global-set-key "\C-x\C-b" 'buffer-menu)
 (global-set-key "\C-xw" 'whitespace-mode)
 (global-set-key "\C-cw" 'tmux-copy)
 (global-set-key "\C-x\C-r" (lambda () (interactive) (revert-buffer t t)))
+(global-set-key "\M-g" 'goto-line)
 (global-set-key "\C-c\C-c" 'comment-region)
-(global-set-key (kbd "C-c w") 'tmux-copy)
-(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
 (global-set-key "\C-ct" 'testify-run-test)
 (global-set-key "\C-c\C-o" 'testify-run-case)
 (global-set-key "\C-c1" 'evil-mode)
+(global-set-key (kbd "C-c w") 'tmux-copy)
+(global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
+(global-set-key "\C-ct" 'testify-run-test)
+(global-set-key "\C-c\C-t" 'testify-run-case)
 
 ;; Something will occasionally override this binding.
 (global-set-key "\C-cg" 'rope-goto-definition)
@@ -157,7 +175,7 @@
 ;;   (package-refresh-contents))
 
 ;; ;; Add in your own as you wish:
-;; (defvar my-packages '(starter-kit starter-starter-kit-bindings)
+;; (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
 ;;   "A list of packages to ensure are installed at launch.")
 
 ;; (dolist (p my-packages)
@@ -220,24 +238,8 @@
 
 (load-file "~/.emacs.d/emacs-for-python/epy-init.el")
 
-(setq skeleton-pair nil) ;; This breaks pasting from os clipboard
+(setq skeleton-pair nil) ;; This stuff sucks.
 
 ;; =============================================================================
 ;;                                                                     Customize
 ;; =============================================================================
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#e4e4e4" "#d70000" "#5f8700" "#af8700" "#0087ff" "#af005f" "#00afaf" "#808080"])
- '(ansi-term-color-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
- '(background-color "#ffffd7")
- '(background-mode light)
- '(cursor-color "#626262")
- '(custom-safe-themes (quote ("762d33a7e24260c48c6b16381d013bf86d8a0fd918700c702f35d0c7ee59b689" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "36a309985a0f9ed1a0c3a69625802f87dee940767c9e200b89cdebdb737e5b29" default)))
- '(fci-rule-color "#383838")
- '(foreground-color "#626262")
- '(safe-local-variable-values (quote ((python-indent . tab-width) (whitespace-line-column . 80) (lexical-binding . t)))))
-
